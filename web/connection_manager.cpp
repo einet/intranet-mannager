5c77826faa9cb14f2c1a61baf3c4c062eddd2ce2
iitrip <zhangliuxue@hotmail.com>
2015-12-07 11:08:54 +0800

    连接管理加锁

--- web/connection_manager.cpp
+++ web/connection_manager.cpp
@@ -7,33 +7,37 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
-
+//2015-12-07 增加写锁，发现有崩溃现象
 #include "connection_manager.hpp"
-
+#include <boost/thread/locks.hpp>
+#include <boost/thread/pthread/shared_mutex.hpp>
+typedef boost::shared_mutex connection_Lock;
+typedef boost::unique_lock<connection_Lock> WriteConnLock;
+typedef boost::shared_lock<connection_Lock> ReadConnLock;
+connection_Lock connUnLock;
 namespace http {
 namespace server {
 
-connection_manager::connection_manager()
-{
+connection_manager::connection_manager() {
 }
 
-void connection_manager::start(connection_ptr c)
-{
-  connections_.insert(c);
-  c->start();
+void connection_manager::start(connection_ptr c) {
+	WriteConnLock W_lock(connUnLock);
+	connections_.insert(c);
+	c->start();
 }
 
-void connection_manager::stop(connection_ptr c)
-{
-  connections_.erase(c);
-  c->stop();
+void connection_manager::stop(connection_ptr c) {
+	WriteConnLock W_lock(connUnLock);
+	connections_.erase(c);
+	c->stop();
 }
 
-void connection_manager::stop_all()
-{
-  for (auto c: connections_)
-    c->stop();
-  connections_.clear();
+void connection_manager::stop_all() {
+	WriteConnLock W_lock(connUnLock);
+	for (auto c : connections_)
+		c->stop();
+	connections_.clear();
 }
 
 } // namespace server
